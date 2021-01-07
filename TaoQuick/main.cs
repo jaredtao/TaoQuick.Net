11a8635470ec9b6a3a4b1db8a1bbd6b12eb693f5
jared <jared@milesight.com>
2021-01-07 09:06:23 +0800

    init version

---
+++ TaoQuick/main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaoQuick_DotNet
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
